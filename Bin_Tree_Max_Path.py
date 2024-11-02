160beed292399735c02a4545c4f3cc43950ac097
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2024-11-02 11:57:13 -0700

    Short sol

--- Bin_Tree_Max_Path.py
+++ Bin_Tree_Max_Path.py
@@ -1,79 +1,36 @@
 from queue import Queue
 import math
+from queue import Queue
+import math
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 class Solution:
+    def dfs(self, root):
+        if not root:
+            return 0
 
-    def get_list(self, root) -> list:
-        # Allow for 1 indexing
-        res = [None]
-        q = Queue()
-        q.put(root)
+        left_max = max(self.dfs(root.left), 0) 
+        right_max = max(self.dfs(root.right), 0)
 
-        while not q.empty():
-            curr_node = q.get()
-            if curr_node:
-                res.append(curr_node.val)
-            else:
-                res.append(curr_node)
+        current_path_sum = root.val + left_max + right_max
 
-            if curr_node:
-                q.put(curr_node.left)
-                q.put(curr_node.right)
+        self.max_sum = max(self.max_sum, current_path_sum)
 
-        return res
+        return root.val + max(left_max, right_max)
 
     def maxPathSum(self, root) -> int:
-        # Stores path distances a node and its descendant
-        tree_list = self.get_list(root)
-        # Lowest possible sum is 
-        # 3 * 1e4 * -1000, so this value has some buffer
-        max_sum = 4 * 1e4 * -1000
-        print(f"Tree list: {tree_list}")
+        self.max_sum = float('-inf')
+        self.dfs(root)
+        return self.max_sum
 
-        tree_indices = []
-        for idx, val in enumerate(tree_list):
-            if val != None:
-                tree_indices.append(idx)
 
-        # DP table where the keys are tuples, and the value is
-        # the path cost
-        dp = {}
-
-        for outer in range(len(tree_indices)):
-            for inner in range(outer, len(tree_indices)):
-                i = tree_indices[outer]
-                j = tree_indices[inner]
-
-                i_depth = int(math.log2(i)) + 1
-                j_depth = int(math.log2(j)) + 1
-                # Same node -> path cost of 0
-                if i == j:
-                    dp[(i, j)] = tree_list[i]
-                # Share the same parent -> sum of their path costs
-                # and their parent
-                elif i / 2 == j / 2:
-                    dp[(i, j)] = tree_list[i] + tree_list[j] + tree_list[i // 2]
-                # Same depth, but not same parent -> move up
-                # and add current costs
-                elif i_depth == j_depth:
-                    dp[(i, j)] = tree_list[i] + tree_list[j] + dp[(i // 2, j // 2)]
-                # i has greater depth than j -> move i up, leave j where it is
-                elif i_depth > j_depth:
-                    dp[(i, j)] = tree_list[i] + dp[(i // 2, j)]
-                # j has greater depth than i -> move j up, leave i where it is
-                else:
-                    dp[(i, j)] = tree_list[j] + dp[(i, j // 2)]
-
-                if dp[(i, j)] > max_sum:
-                    max_sum = dp[(i, j)]
-
-        return max_sum
 
 def main():
     r2 = TreeNode(7, None, None)
