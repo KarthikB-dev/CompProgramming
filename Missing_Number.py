dd44b8eef43ba35dee83083f79e5affb87b4121d
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2024-11-06 00:01:36 -0800

    2 line O(n) sol

--- Missing_Number.py
+++ Missing_Number.py
@@ -1,12 +1,6 @@
 from typing import List
 
-
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        if nums[0] != 0:
-            return 0
-        for idx in range(len(nums) - 1):
-            if nums[idx] == nums[idx + 1] - 2:
-                return nums[idx] + 1
-        return nums[-1] + 1
+        est_sum = ((len(nums) + 1) * len(nums)) // 2
+        return est_sum - sum(nums)
