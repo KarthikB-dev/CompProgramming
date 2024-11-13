561a5f471540bb4e7ac54481f48f7fcb4f31a149
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2024-11-12 17:36:29 -0800

    Better algo: top 25% by runtime

--- ValidWordSquare.py
+++ ValidWordSquare.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def validWordSquare(self, words: List[str]) -> bool:
-        cols = []
         for cidx in range(len(words[0])):
             new_col = ""
             for ridx in range(len(words)):
                 if cidx < len(words[ridx]):
                     new_col += words[ridx][cidx]
-            cols.append(new_col)
+            if new_col != words[cidx]:
+                return False
 
-        return cols == words
+        return True
